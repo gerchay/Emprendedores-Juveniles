4981a28e324a603c7c7eb3ce9c05c4bd4cdc264c
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2021-03-10 21:58:15 -0600

    Se crea modelo de usuario y funciones para registrar y loguearse

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,9 @@
+export interface USUARIO{
+  uid?:string,
+  cui?:string,
+  carnet?:number,
+  nombres?:string,
+  apellidos?:string,
+  correo?:string,
+  telefono?:number,
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { USUARIO } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,25 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   userData$: Observable<any>;
-  constructor( private afAuth: AngularFireAuth ) { this.userData$ = this.afAuth.authState; }
+  constructor( private afAuth: AngularFireAuth, private firestore: AngularFirestore, ) { this.userData$ = this.afAuth.authState; }
   onLogin = ({ email, password }) => this.afAuth.signInWithEmailAndPassword( email, password );
-  logout = () => this.afAuth.signOut()
+  logout = () => this.afAuth.signOut();
+
+  async onRegister( usuario:USUARIO, password:string ){
+    try {
+      let respuesta:boolean;
+      const { user } = await this.afAuth.createUserWithEmailAndPassword( usuario.correo, password );
+      await user.updateProfile({ displayName: usuario.nombres });
+      await this.firestore.collection( 'usuarios' ).doc( user.uid ).set( Object.assign({}, usuario) )
+        .then( resp => respuesta = true )
+        .catch( err =>{ respuesta = false; console.error(err); } )
+
+      return respuesta || false;
+
+    } catch (error) {
+      console.error('Error on register', error);
+      if(error.message == 'The email address is already in use by another account.') console.log( usuario.correo );
+      return false;
+    }
+  }
 }
