14f98b9b6e3c236d51577906a33bcd48626dee72
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2021-03-10 20:51:28 -0600

    Creación de services y guard auth

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  userData$: Observable<any>;
+  constructor( private afAuth: AngularFireAuth ) { this.userData$ = this.afAuth.authState; }
+  onLogin = ({ email, password }) => this.afAuth.signInWithEmailAndPassword( email, password );
+  logout = () => this.afAuth.signOut()
+}
